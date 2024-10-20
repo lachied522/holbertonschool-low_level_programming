82f5299ed1efbca0987d276b445f8798c3b6869f
root <lachie.duncan12@gmail.com>
2024-10-20 23:55:48 +0000

    Printing without stdio

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - Prints a string without using stdio
+ * Return: void
+ */
+
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (0);
+}
