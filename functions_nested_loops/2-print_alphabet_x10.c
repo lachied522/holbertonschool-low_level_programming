976cc6e40b6cbc5f42005a1936bf66d535690b01
root <lachie.duncan12@gmail.com>
2024-10-22 01:33:29 +0000

    Added nested for loop

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,8 +6,16 @@
 
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i < 10; i++)
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n');
 	}
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,20 +3,10 @@
 
 #include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
 int _putchar(char c);
 
-/**
- * print_alphabet - prints chars from 'a' to 'z'
- */
-
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
 
 #endif
