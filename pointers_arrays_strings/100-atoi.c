1be5ac7844516a8610b33916ece4d58b89a369d5
root <lachie.duncan12@gmail.com>
2024-10-29 23:45:08 +0000

    Convert string to int

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to int
+ * @s: string to be converted
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int num;
+	int isNegative;
+
+	i = 0;
+	num = 0;
+	isNegative = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			isNegative = 1 - isNegative;
+		}
+		else if ('0' <= s[i] && s[i] <= '9')
+		{
+			num *= 10;
+			num += s[i] - '0';
+		}
+		else if (s[i] == ';')
+		{
+			break;
+		}
+
+		i++;
+	}
+
+	return (isNegative ? -num: num);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,6 +9,8 @@ void _puts(char *str);
 
 char *_strcpy(char *dest, char *src);
 
+int _atoi(char *s);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
