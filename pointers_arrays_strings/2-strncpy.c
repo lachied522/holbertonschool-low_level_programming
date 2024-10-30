64065aa61a5cbf5938270c676290443a2d296442
root <lachie.duncan12@gmail.com>
2024-10-30 02:14:38 +0000

    Copy string up to n bytes

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n bytes from src string to dest string
+ * @src: pointer to src string
+ * @dest: pointer to destination string
+ * @n: integer, number of bytes from src to copy
+ * Return: buffer containing concatenated string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,6 +15,8 @@ char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
 
+char *_strncpy(char *dest, char *src, int n);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
