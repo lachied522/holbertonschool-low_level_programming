646a89196d5975a3e05512a36411f9c5299553e1
root <lachie.duncan12@gmail.com>
2024-11-03 21:44:12 +0000

    Set memory to constant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - fills first n bytes of memory with byte b
+ * @s: pointer to memory area
+ * @b: byte to populate memory with
+ * @n: number of memory bytes to fill
+ * Returns: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,6 +19,8 @@ char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
+char *_memset(char *s, char b, unsigned int n);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
