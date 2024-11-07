bf8a23f68ef4fb3d5a84f3a4d5b4267e85ad51c8
root <lachie.duncan12@gmail.com>
2024-11-07 07:24:29 +0000

    Handled non-digit characters

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - add positive integers
@@ -11,18 +12,30 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int d;
+	int j;
+	int isNumber;
 	int sum;
 
 	for (i = 1; i < argc; i++)
 	{
-		d = atoi(argv[i]);
-		if (d < 1)
+		isNumber = 1;
+		for (j = 0; j < strlen(argv[i]); j++)
+		{
+			if (
+				argv[i][j] != '-' &&
+				!('0' <= argv[i][j] && argv[i][j] <= '9')
+			)
+			{
+				isNumber = 0;
+				break;		
+			}
+		}
+		if (isNumber == 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += d;
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
