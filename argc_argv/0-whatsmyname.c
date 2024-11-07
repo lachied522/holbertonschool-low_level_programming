83fd09c4aea4cd2d33448b25ffb3db0edc5329a9
root <lachie.duncan12@gmail.com>
2024-11-07 05:52:38 +0000

    Print program name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print name of program, followed by new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return - 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
