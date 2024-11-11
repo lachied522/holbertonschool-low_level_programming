dd2c495950ffa18af3109ed2985b0a299d560f2a
root <lachie.duncan12@gmail.com>
2024-11-11 00:26:32 +0000

    Trimmed below 40 lines

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,45 +14,30 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i;
 	unsigned int size1;
 	unsigned int size2;
-
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-	size1 = 0;
 	i = 0;
-
+	size1 = 0;
 	while (s1[i] != '\0')
 	{
 		size1++;
 		i++;
 	}
-
-	size2 = 0;
 	i = 0;
-
+	size2 = 0;
 	while (s2[i] != '\0')
 	{
 		size2++;
 		i++;
 	}
-
 	buffer = malloc(size1 + size2 + 1);
-
 	if (buffer == NULL)
 		return (NULL);
-
 	for (i = 0; i < size1; i++)
-	{
 		buffer[i] = s1[i];
-	}
-
 	for (i = 0; i < size2 + 1; i++)
-	{
 		buffer[size1 + i] = s2[i];
-	}
-
 	return (buffer);
 }
