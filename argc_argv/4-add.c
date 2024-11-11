d02b1135c30b256b1da782cfef05ae785d7bc604
root <lachie.duncan12@gmail.com>
2024-11-11 23:39:37 +0000

    Add numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,30 +13,21 @@ int main(int argc, char *argv[])
 {
 	int i;
 	char *s;
-	int isNumber;
 	int sum;
 
 	for (i = 1; i < argc; i++)
 	{
-		isNumber = 1;
 		s = argv[i];
 		while (*s != '\0')
 		{
-			if (
-				*s != '-' &&
-				!('0' <= *s && *s <= '9')
-			)
+			if (!('0' <= *s && *s <= '9'))
 			{
-				isNumber = 0;
-				break;		
+				printf("Error\n");
+				return (1);
 			}
 			s++;
 		}
-		if (isNumber == 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
+
 		sum += atoi(argv[i]);
 	}
 
