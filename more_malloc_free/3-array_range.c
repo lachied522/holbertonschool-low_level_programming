b2e42eb06a7e382996cf5712da3a85d5d403a06b
root <lachie.duncan12@gmail.com>
2024-11-13 22:28:01 +0000

    Array arrange

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_arrange - creates array of ints from min to max
+ * @min: int
+ * @max: int
+ * Return: pointer to array or NULL
+ */
+
+int *array_range(int min, int max)
+{
+	int *buffer;
+	unsigned int i;
+
+	if (min > max)
+		return (NULL);
+
+	buffer = malloc((max - min) * sizeof(int));
+
+	if (buffer == NULL)
+		return (NULL);
+
+	for (i = min; i < max; i++)
+		buffer[i - min] = i;
+
+	return (buffer);
+}
