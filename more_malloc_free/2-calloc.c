04f5ece78a6c09fa16f4f15e359267a78ec6634b
root <lachie.duncan12@gmail.com>
2024-11-13 23:50:14 +0000

    Switched to char array

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,8 +10,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *buffer;
-	int *ptr;
+	char *buffer;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,12 +21,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (buffer == NULL)
 		return (NULL);
 
-	ptr = buffer;
-	while (*ptr)
-	{
-		*ptr = 0;
-		ptr++;
-	}
+	for (i = 0; i < nmemb * size; i++)
+		buffer[i] = 0;
 
 	return (buffer);
 }
