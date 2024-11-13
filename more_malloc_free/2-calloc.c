b1aa28abc8e8dbc2f28c59a1e7d8516259205358
root <lachie.duncan12@gmail.com>
2024-11-13 22:05:00 +0000

    Added sizeof

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,14 +11,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *buffer;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	buffer = malloc(nmemb * size);
+	buffer = malloc(nmemb * size * sizeof(unsigned int));
 
 	if (buffer == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		buffer[i] = 0;
 
 	return (buffer);
 }
