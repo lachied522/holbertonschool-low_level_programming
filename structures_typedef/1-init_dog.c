1f6720385a07c212cd896ebeeb378eca1af5bd15
root <lachie.duncan12@gmail.com>
2024-11-16 03:38:46 +0000

    Separate function definition

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialise a variable of type dog
+ * @d: pointer to dog struct
+ * @name: name string
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+}
