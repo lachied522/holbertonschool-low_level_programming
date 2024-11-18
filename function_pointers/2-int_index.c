7d9f54cbf20f731605cf9f7e52fad73b3500332a
root <lachie.duncan12@gmail.com>
2024-11-18 03:09:38 +0000

    Find int in array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * nt_index - searches for integer in array
+ * @array: array to search through
+ * @size: size of array
+ * @cmp: function that returns 0 for desired integer
+ * Return: index of first occurence of integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
