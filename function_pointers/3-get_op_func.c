0754e38891a5955329a506ca81b60985338c83cb
root <lachie.duncan12@gmail.com>
2024-11-18 04:29:30 +0000

    Added strcmp to compare operators

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - selects function to perform based on operator
@@ -21,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0])
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
