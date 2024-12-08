685a7d6708a0716ff70881569ae4bdd497dfaec6
root <lachie.duncan12@gmail.com>
2024-12-08 01:53:32 +0000

    Get bit at index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_bit - returns bit at index
+ * @n: integer
+ * @index: index of bit to return
+ * Return: bit at index or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int selector = 1 << index;
+	unsigned long int intersection = n & selector;
+	return (intersection >> index);
+}
